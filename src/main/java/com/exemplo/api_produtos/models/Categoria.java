62f220c239a326b9ee60da16ec1f4cd12410c139
SARA LAMARI SILVA <sara.silva591@etec.sp.gov.br>
2025-11-17 10:15:06 -0300

    feat: initial commit

---
+++ src/main/java/com/exemplo/api_produtos/models/Categoria.java
@@ -0,0 +1,32 @@
+package com.exemplo.api_produtos.models;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    private List<Produto> produtos;
+
+    public Categoria(){}
+
+    public Categoria(String nome, List<Produto> produtos) {
+        this.nome = nome;
+        this.produtos = produtos;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome( String nome ) { this.nome = nome; }
+    public List<Produto> getProdutos () {return produtos; }
+    public void setProdutos (List<Produto> produtos ) {this.produtos = produtos; }
+}
---
+++ src/main/java/com/exemplo/api_produtos/models/Fornecedor.java
@@ -0,0 +1,33 @@
+package com.exemplo.api_produtos.models;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_fornecedores")
+public class Fornecedor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToMany(mappedBy = "fornecedores")
+    private Set<Produto> produtos;
+
+    public Fornecedor() {}
+
+    public Fornecedor(String nome, Set<Produto> produtos){
+        this.nome = nome;
+        this.produtos = produtos;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome( String nome ) { this.nome = nome; }
+    public Set<Produto> getProdutos () {return produtos; }
+    public void setProdutos (Set<Produto> produtos ) {this.produtos = produtos; }
+}
